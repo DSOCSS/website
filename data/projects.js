7da74c92ee2929344d0c9c7418df2fb49293c901
A Zarins <ar.zarins@gmail.com>
2024-02-02 21:37:09 -0500

    Week 2 puzzles

---
+++ data/projects.js
@@ -0,0 +1,36 @@
+export let projectData = [
+    {
+        "name": "Gradescope Average",
+        "year": "'24 Spring",
+        "team": ["Connor Zhang '27", "Arthur Zarins '27"],
+        "description": `Gradescope is a popular website for releasing individual assignment grades in math classes, but doesn't show students their overall grade. This program quickly calculates your average grade for you!`,
+        "link": "https://dsocss.github.io/GradescopeAverage/",
+        "image": "../images/projects/gradescope_avg.png"
+
+    },
+    {
+        "name": "Conway 2 Player",
+        "year": "'24 Spring",
+        "team": ["Ribhu Hooja '27", "Arthur Zarins '27"],
+        "description": `Conway 2 Player is a strategic game based off of John Conway's Game of Life. Take turns adding cells to the board and compete to have your team dominate the board!`,
+        "link": "https://dsocss.github.io/Conway2Player/",
+        "image": "../images/projects/Conway2Player.png"
+
+    },
+    {
+        "name": "Sudoku Solver",
+        "year": "'24 Winter",
+        "team": ["Arthur Zarins '27", "Nate Abbott '27"],
+        "description": `Stuck trying to solve a sudoku puzzle? With the sudoku solver, you can quickly type a sudoku board and have the program solve it for you! The solver uses a scanning-elimination technique and depth-first-search backtracking to find a solution.`,
+        "link": "https://dsocss.github.io/sudoku-solver/",
+        "image": "../images/projects/SudokuSolver.png"
+    },
+    {
+        "name": "Linear Equation Solver",
+        "year": "'24 Winter",
+        "team": ["Arthur Zarins '27", "Muneeb Chaudhary '27"],
+        "description": `The linear equation solver will find the solution space to any given system of equations by the user, in the form of of a matrix. It can convert any matrix into reduced row echelon form, and calculates the determinant and inverse of a square matrix.`,
+        "link": "https://github.com/DSOCSS/linear-equation-solver",
+        "image": "../images/projects/LinearEquations.png"
+    }
+];
