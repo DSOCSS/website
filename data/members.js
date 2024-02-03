a334a207ffd1a3ec8f3efc0353dd16408299b41b
A Zarins <ar.zarins@gmail.com>
2024-02-02 21:37:09 -0500

    Update team page

---
+++ data/members.js
@@ -0,0 +1,58 @@
+export let teamData = [
+    {
+        "name": "Arthur Zarins",
+        "year": 2027,
+        "bio": "I am a '27 from Davis, California studying computer science. Aside from CS Society, I can be found solving twisty puzzle variants, skiing at the Dartmouth Skiway, and spending time with my labrador retriever, Abby.",
+        "image": "../images/members/arthurZarins.jpeg",
+        "role": "President",
+        "gravity": 0,
+        "website": "https://arthurzarins.com",
+        "favorite_class": ""
+    },
+    {
+        "name": "Nate Abbott",
+        "year": 2027,
+        "bio": "",
+        "image": "../images/members/nateAbbott.jpeg",
+        "role": "Treasurer",
+        "gravity": 0,
+        "favorite_class": ""
+    },
+    {
+        "name": "Abby",
+        "bio": "",
+        "image": "../images/members/abby.jpeg",
+        "role": "Mascot",
+        "gravity": 0,
+        "website": "https://dsocss.github.io/AbbysWebsite/",
+        "favorite_class": ""
+    },
+    {
+        "name": "Ribhu Hooja",
+        "year": 2027,
+        "bio": "",
+        "image": "../images/members/ribhuHooja.jpeg",
+        "role": "Education Lead",
+        "gravity": 0,
+        "website": "https://ribhuhooja.me",
+        "favorite_class": ""
+    },
+    {
+        "name": "Rhea Karty",
+        "year": 2025,
+        "bio": "",
+        "image": "../images/members/rheaKarty.jpeg",
+        "role": "Event Coordinator",
+        "gravity": 0,
+        "favorite_class": ""
+    }
+    // {
+    //     "name": "",
+    //     "year": 0,
+    //     "bio": "",
+    //     "image": "",
+    //     "role": "",
+    //     "gravity": 0,
+    //     "favorite_class": ""
+    // }
+];
